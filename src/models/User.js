67c5220bc8d18664f10b2be23054e0bdc996e83e
MishraAmit1 <amsmishra1510@gmail.com>
2025-05-14 01:13:22 +0530

    final checking handling

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   name: { type: String },
-  role: { type: String, enum: ["guest", "admin"], default: "guest" },
+  role: { type: String, enum: ["guest", "admin", null], default: null },
   createdAt: { type: Date, default: Date.now },
 });
 
